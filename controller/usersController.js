c491b459eeb85f97b1d1a09396cec75f427cf24d
MshSiam <siyamkenway@gmail.com>
2022-07-22 16:34:31 +0600

    form data created

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,9 +1,46 @@
+const bcrypt = require("bcrypt")
+
 function getUsers(req, res, next) {
   res.render("users", {
     title: "Users - Chat Application"
   })
 }
 
+// add user
+async function addUser(req, res, next) {
+  let newUser
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  if (req.files && req.files.length > 0) {
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword
+    })
+  } else {
+    newUser = new User({
+      ...req.body,
+      password: hashedPassword
+    })
+  }
+
+  // save the user
+  try {
+    const result = await newUser.save()
+    res.status(200).json({
+      message: "User created successfully."
+    })
+  } catch (error) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: "Could not delete the user!"
+        }
+      }
+    })
+  }
+}
+
 module.exports = {
-  getUsers
+  getUsers,
+  addUser
 }
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,10 +1,22 @@
 //  imports//
 const express = require("express")
-const { getUsers } = require("../controller/usersController")
+const { getUsers, addUser } = require("../controller/usersController")
 const decorateHtmlResponse = require("../middleweres/common/decorateHtmlResponse")
-
+const avatarUpload = require("../middleweres/users/avatarUpolad")
+const {
+  addUserValidators,
+  addUserValidationHandler
+} = require("../middleweres/users/usersValidator")
 const router = express.Router()
 
+// ========= all routes =============//
 router.get("/", decorateHtmlResponse("Users"), getUsers)
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+)
 
 module.exports = router
